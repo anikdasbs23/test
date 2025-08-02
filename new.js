f0eb2572842c50c56f272937070d4ab808952464
Anik66 <anikkumardas966@gmail.com>
2025-08-02 22:37:31 +0600

    branch commit

---
+++ new.js
@@ -0,0 +1,17 @@
+// 1. Variables
+let name = "Anik";
+const age = 25;
+
+// 2. Function
+function greet(user) {
+  return "Hello, " + user + "!";
+}
+
+// 3. Conditional Statement
+if (age >= 18) {
+  console.log(greet(name));
+} else {
+  console.log("You are underage.");
+}
+
+// 4. Loo
